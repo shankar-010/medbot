75bc6fe14bbd2efb787f309e84666b1595a6b948
shankar-010 <shankar.namaji3@gmail.com>
2025-08-25 22:06:31 +0530

    first commit

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -40,7 +40,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-
+router.post('/diagnose', authController.diagnose); // Add this line
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 
